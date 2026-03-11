30013eea5901cd304cc8f3cd73c30853fb87e1ff
serena <serena.chang169@gmail.com>
2026-03-11 16:59:52 +0800

    update

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -3,15 +3,15 @@ module CustomersHelper
     if current_sort == desc_key
       next_sort = asc_key
       arrow = " ▼"
-      arrow_style = "color:inherit;"
+      arrow_style = ""
     elsif current_sort == asc_key
       next_sort = desc_key
       arrow = " ▲"
-      arrow_style = "color:inherit;"
+      arrow_style = ""
     else
       next_sort = desc_key
-      arrow = " ↕"
-      arrow_style = "color:inherit; opacity:0.4;"
+      arrow = " ⇅"
+      arrow_style = "opacity:0.6;"
     end
     link_to customers_path(request.query_parameters.merge(sort: next_sort, page: 1)),
       style: "color:inherit; text-decoration:none;" do
