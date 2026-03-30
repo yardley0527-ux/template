e6f5eaf38b86d10099810f4987e869a7752976cd
serena <serena.chang169@gmail.com>
2026-03-30 16:25:09 +0800

    update

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,30 +1,20 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data,
+                     "https://res.cloudinary.com",
+                     "https://upload-widget.cloudinary.com"
+  policy.object_src  :none
+  policy.style_src   :self, :https, :unsafe_inline
 
-# Define an application-wide content security policy
-# For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+  policy.script_src  :self, :https,
+                     "https://upload-widget.cloudinary.com"
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
-#   # If you are using webpack-dev-server then specify webpack-dev-server host
-#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+  policy.connect_src :self, :https,
+                     "https://api.cloudinary.com",
+                     "https://res.cloudinary.com",
+                     "https://upload-widget.cloudinary.com"
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
-
-# If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-
-# Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+  # ⬇️ 這行最關鍵，Widget 是用 iframe 渲染的
+  policy.frame_src   "https://upload-widget.cloudinary.com"
+end
